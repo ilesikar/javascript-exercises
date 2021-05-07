08bfe59e1e4821b514586df5d293f12a13f0980d
Ian Lesikar <ilesikar@gmail.com>
2021-05-07 03:01:27 -0700

    finished palindromes and calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,48 @@
-function add () {
-	
+function add (num1, num2) {
+	return num1 + num2;	
 }
 
-function subtract () {
-	
+function subtract (num1, num2) {
+	return num1 - num2;
 }
 
-function sum () {
-	
+function sum (nums) {
+	let result = 0;
+	if (nums.length) {
+		result = nums.reduce((acc, num) => {
+			return acc + num;
+		},0);
+	}
+	return result;
 }
 
-function multiply () {
-	
+function multiply (nums) {
+	let result = 0;
+	if (nums.length) {
+		result = nums.reduce((acc, num) => {
+			return acc * num;
+		},1);
+	}
+	return result;
 }
 
-function power() {
-	
+function power(num, exponent) {
+	return Math.pow(num, exponent);
 }
 
-function factorial() {
-	
+function factorial(num) {
+	if (num === 0) return 1;
+	let nums = [];
+	let result = 0;
+	for (i = 1; i <= num; i++) {
+		nums.push(i);
+	}
+	if (nums.length) {
+		result = nums.reduce((acc, num) => {
+			return acc*num;
+		},1);
+	}
+	return result;
 }
 
 module.exports = {
--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+    const removePunctuation = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    const noSpaces = removePunctuation.split(' ').join('');
+    const noCapitals = noSpaces.toLowerCase();
+    for (let char in noCapitals) {
+        if (noCapitals[char] != noCapitals[noCapitals.length-char-1]) {
+            return false;
+        }
+    }
+    return true;
 }
+
+palindromes('A! e r t%.');
 
 module.exports = palindromes
