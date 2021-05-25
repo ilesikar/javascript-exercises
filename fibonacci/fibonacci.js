91e647c462efe958df7babc30d1bf4bcb5ce56df
Ian Lesikar <ilesikar@gmail.com>
2021-05-25 14:01:37 -0700

    fibonacci finished

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    if (input <= 0) return 'OOPS';
+    input = Number(input)
+    let sum = 0
+    let current = 1;
+    let previous = 0;
+    for (let i=0; i < input - 1; i++) {
+        sum = current + previous;
+        previous = current;
+        current = sum;
+    }
+    return current;
 }
 
 module.exports = fibonacci
